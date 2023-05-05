865a5a5dbfbd0be8e9478a264262341474aac2e2
anvaka <anvaka@gmail.com>
2023-05-04 20:14:36 -0700

    use main branch

--- deploy-demo.sh
+++ deploy-demo.sh
@@ -6,7 +6,7 @@ cd ./dist
 git init
 git add .
 git commit -m 'push to gh-pages'
-git push --force git@github.com:anvaka/streamlines.git master:gh-pages
+git push --force git@github.com:anvaka/streamlines.git main:gh-pages
 cd ../
 git tag `date "+demo-%Y%m%d%H%M%S"`
 git push --tags
